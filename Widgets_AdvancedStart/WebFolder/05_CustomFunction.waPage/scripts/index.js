bbcb6bea466c098d0d32ed88f23cd8a477e140f7
Greg McCarvell <greg@gregmccarvell.com>
2012-12-18 21:44:42 -0500

    Custom Function start point modified

---
+++ Widgets_AdvancedStart/WebFolder/05_CustomFunction.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button3 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button3.click = function button3_click (event)// @startlock
+	{// @endlock
+		$$('component1').loadComponent('/components/05_CustomFunction2.waComponent');
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button3", "click", button3.click, "WAF");
+// @endregion
+};// @endlock
---
+++ Widgets_AdvancedStart/WebFolder/components/05_CustomFunction2.waComponent/05_CustomFunction2.js
@@ -0,0 +1,31 @@
+﻿
+(function Component (id) {// @lock
+
+// Add the code that needs to be shared between components here
+
+function constructor (id) {
+
+	// @region beginComponentDeclaration// @startlock
+	var $comp = this;
+	this.name = '05_CustomFunction2';
+	// @endregion// @endlock
+	this.myFunction = function(){
+		alert('this is a completely different function');
+	}
+	
+	this.load = function (data) {// @lock
+
+	// @region namespaceDeclaration// @startlock
+	// @endregion// @endlock
+
+	// eventHandlers// @lock
+
+	// @region eventManager// @startlock
+	// @endregion// @endlock
+
+	};// @lock
+
+
+}// @startlock
+return constructor;
+})();// @endlock
