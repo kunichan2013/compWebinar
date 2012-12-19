b8bc076818ca51afcad40f44d036cec9f70245b8
Greg McCarvell <greg@gregmccarvell.com>
2012-12-18 22:28:07 -0500

    Dynamic Creation start point modified

---
+++ Widgets_AdvancedStart/WebFolder/06_DynamicCreation.waPage/scripts/index.js
@@ -0,0 +1,39 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var createTabButton = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	createTabButton.click = function createTabButton_click (event)// @startlock
+	{// @endlock
+		$$('tabView1').addTab($$('tabView1').countTabs(), true);
+		var currentTab = $$('tabView1').getSelectedTab();
+		var tabContainerID = $$('tabView1').getTabContainer(currentTab.index).id;
+		
+		var domID = 'c' + Math.floor(Math.random()*100000 + 1);
+		var componentDOM = document.createElement('div');
+		componentDOM.setAttribute('id', domID);
+		document.body.appendChild(componentDOM);
+     
+		var componentWidget = new WAF.widget.Component({
+			'id': domID,
+			'data-type': 'component',
+			'data-lib': 'WAF',
+			'data-constraint-top': 'true',
+			'data-constraint-right': 'true',
+			'data-constraint-left': 'true',
+			'data-constraint-bottom': 'true',
+			'class': 'waf-widget waf-component default inherited'
+		});
+		
+		$$(tabContainerID).addChild(componentWidget);
+		componentWidget.loadComponent($$('tabType').getValue());
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("createTabButton", "click", createTabButton.click, "WAF");
+// @endregion
+};// @endlock
